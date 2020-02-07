868864c8a3decdf7cf95fe5a76857bbf7757d28a
1mikegrn <1mikegrn@gmail.com>
2020-02-07 11:56:44 -0800

    added colab functionality

--- CitrineChallenge/src/constraints.py
+++ CitrineChallenge/src/constraints.py
@@ -9,7 +9,7 @@ class Constraint():
         """
 
         if 'google' in kwargs and kwargs['google'] is True:
-            lines = fname
+            lines = [x for x in fname.decode().split('\n') if x != '']
         
         else:
             with open(fname, "r") as f:
