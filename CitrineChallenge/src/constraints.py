a3bd2cafb0bd9a67ef5f4cc347ac029fe716d959
1mikegrn <1mikegrn@gmail.com>
2020-02-07 11:14:58 -0800

    fixing colab interaction

--- CitrineChallenge/src/constraints.py
+++ CitrineChallenge/src/constraints.py
@@ -9,7 +9,7 @@ class Constraint():
         """
 
         if 'google' in kwargs and kwargs['google'] is True:
-            lines = fname.decode().split('\n')
+            lines = [x for x in fname.decode().split('\n') if x != '']
         
         else:
             with open(fname, "r") as f:
