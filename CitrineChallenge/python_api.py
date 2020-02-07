3f9c064e04250b431db817cdbd5bb06e57b066cd
1mikegrn <1mikegrn@gmail.com>
2020-02-07 10:29:18 -0800

    initial commit

--- CitrineChallenge/python_api.py
+++ CitrineChallenge/python_api.py
@@ -2,7 +2,7 @@ from CitrineChallenge.src.calculator import calculate
 
 def analyze(input_file, n_results):
 
-    """Test docstring"""
+    """Provides Colab enter point for calculation"""
 
     report_values = calculate(input_file, n_results)
 
