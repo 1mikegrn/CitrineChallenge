14a6ba1138252c224963c5b02c7c21c0ce30f739
1mikegrn <1mikegrn@gmail.com>
2020-02-06 17:21:20 -0800

    remove pycache?

--- src/tools/quick_graphs.py
+++ src/tools/quick_graphs.py
@@ -2,7 +2,6 @@ import mpl_toolkits.mplot3d.axis3d as axis3d
 
 from matplotlib import pyplot as plt
 
-
 def dim3(report_values):
     
     fig = plt.figure()
@@ -10,3 +9,7 @@ def dim3(report_values):
 
     ax.scatter(report_values[:,2], report_values[:,1],report_values[:,0])
     plt.show()
+
+def dim2(report_values):
+
+    pass
